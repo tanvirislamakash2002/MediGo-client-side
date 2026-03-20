137ea71d214a5456da5c8e4f20f50e7d7faf1966
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-03-20 20:12:38 +0600

    initialized the project

---
+++ src/app/(CommonLayout)/shop/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopPage = () => {
+    return (
+        <div>
+            This is shop page where you can see all the medicine.
+        </div>
+    );
+};
+
+export default ShopPage;
